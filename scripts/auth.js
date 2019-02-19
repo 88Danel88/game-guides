37430dfaeab90b528517d03e081a293efa0b5321
Danel Tabada <daneltabada@gmail.com>
2019-02-19 08:00:30 +0800

    added code to signOut/logOut users

--- scripts/auth.js
+++ scripts/auth.js
@@ -16,3 +16,13 @@ signupForm.addEventListener('submit', (e) => {
     signupForm.reset();
   });
 });
+
+//log out or sign out a user
+const logout = document.querySelector('#logout');
+  logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+  console.log('user signed out');
+  })
+});
+
