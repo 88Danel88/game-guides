7517083b1f75dcf89bf38cb779d3299ce6fe6bf1
Danel Tabada <danel.tabada@onsemi.com>
2019-02-22 16:32:42 +0800

    ran into issue, fixed code back up to adding user info

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,12 +4,29 @@ auth.onAuthStateChanged(user => {
     db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
       setupUI(user);
-    });
+    }, err => console.log(err.message));
   } else {
     setupUI();
     setupGuides([]);
   }
-})
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
 
 // signup
 const signupForm = document.querySelector('#signup-form');
