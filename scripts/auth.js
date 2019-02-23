67f6b7304ef812565874d7cf5c1f25232d62944e
Danel Tabada <daneltabada@gmail.com>
2019-02-23 10:09:23 +0800

    adding firestore users collection, and displaying bio info

--- scripts/auth.js
+++ scripts/auth.js
@@ -11,6 +11,7 @@ auth.onAuthStateChanged(user => {
   }
 });
 
+
 // create new guide
 const createForm = document.querySelector('#create-form');
 createForm.addEventListener('submit', (e) => {
@@ -37,14 +38,18 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    // close the signup modal & reset form
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
+    // sign up the user & add firestore data
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      return db.collection('users').doc(cred.user.uid).set({
+        bio: signupForm['signup-bio'].value
+      });
+    }).then(() => {
+      // close the signup modal & reset form
+      const modal = document.querySelector('#modal-signup');
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    });
   });
-});
 
 // logout
 const logout = document.querySelector('#logout');
